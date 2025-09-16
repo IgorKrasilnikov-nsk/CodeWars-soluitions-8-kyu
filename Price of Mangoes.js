7f291c933794ce940669fc4c2c71f2cc3c649b43
Igor Krasilnikov <kite84nsk@gmail.com>
2025-09-16 12:41:04 +0700

    Create Price of Mangoes.js

---
+++ Price of Mangoes.js
@@ -0,0 +1,7 @@
+/*
+Accountant time! For a given quantity and price (per mango), calculate the total cost of the mangoes.
+But! Every third mango is free!
+*/
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity/3));
+}
